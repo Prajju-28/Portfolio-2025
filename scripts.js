0795efed1155b87a634261be2d66b8e55bcca26d
PRAJWAL L <prajjuacharya284@gmail.com>
2025-01-28 17:36:26 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// typing animation//
+var typed=new Typed(".typing",{
+    strings:[" ","Full-Stack Developer","Web Designer","Web Developer","Software Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
